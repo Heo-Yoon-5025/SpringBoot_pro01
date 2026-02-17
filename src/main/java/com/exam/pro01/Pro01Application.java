2334b3c011ceae4ac6d9817c945bd03cd154b777
허윤 <huryoun5022@naver.com>
2026-02-17 21:40:38 +0900

    spring boot New Project01

---
+++ src/main/java/com/exam/pro01/Pro01Application.java
@@ -0,0 +1,13 @@
+package com.exam.pro01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro01Application.class, args);
+	}
+
+}
